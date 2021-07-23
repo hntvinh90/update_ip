58d35a1ab0053a9c9fed2b83a7bccf8fdc5a5cc9
root <honguyenthanhvinh@gmail.com>
2021-07-23 09:09:34 +0700

    commit

--- .git-push.sh
+++ .git-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "Updated at $(date)\n$(curl ifconfig.me)" > /root/vinh/update_ip/ip.txt
-git --git-dir=/root/vinh/update_ip/.git --work-tree=/root/vinh/update_ip/ commit -am "commit"
-git --git-dir=/root/vinh/update_ip/.git --work-tree=/root/vinh/update_ip/ push -u origin main
+echo -e "Updated at $(date)\n$(curl ifconfig.me)" > /root/update_ip/ip.txt
+git --git-dir=/root/update_ip/.git --work-tree=/root/update_ip/ commit -am "commit"
+git --git-dir=/root/update_ip/.git --work-tree=/root/update_ip/ push -u origin main
 
